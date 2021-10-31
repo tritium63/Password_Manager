0703fa191b046c15b8ffc811cf931e32bbe7f509
adinmadi <adinmadi@gmail.com>
2021-11-01 01:13:57 +0530

    Synopsis:

--- Currently_Working/Creat_DB.py
+++ Currently_Working/Creat_DB.py
@@ -2,4 +2,5 @@ import mysql.connector
 Connector=mysql.connector.connect(host="localhost", user="root", password="Aditya2004@google")
 Cursor=Connector.cursor()
 Output=Cursor.execute("show DATABASES")
-return(Output)
+x=Cursor.fetchall()
+print(x)
