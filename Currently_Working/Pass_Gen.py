7113bea14792dda2b333c0e38c019ac90456d5cf
Aditya <adityasabkb@gmail.com>
2021-11-04 01:24:45 +0530

    handeled some problem in merge

--- Currently_Working/Pass_Gen.py
+++ Currently_Working/Pass_Gen.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-def Pass_Gen():
-	import random, string
-=======
+
 import random, string
 def Pass_Gen():
->>>>>>> aditya_personal
 	x = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
 	return x
