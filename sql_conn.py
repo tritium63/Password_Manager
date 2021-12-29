ffdb700386d201d55a9cd46f9e91b998089b0521
adinmadi <adinmadi@gmail.com>
2021-12-29 11:23:12 +0530

    Organization

--- sql_conn.py
+++
@@ -1,8 +0,0 @@
-from mysql import connector
-
-
-def Sql_Connection():
-    import mysql.connector
-    Conector=mysql.connector.connect(host="bk3mjn0digzdwyfts1ir-mysql.services.clever-cloud.com", user="umterzn51pcaf9nw", password="sNwAPpmFIKjYG17iwZKJ")
-    #for debuging
-    print("Connected")
