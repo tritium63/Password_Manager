5a91c250add5758c7618cf96dc3e3b4b02354767
Aditya <adityasabkb@gmail.com>
2022-01-01 20:38:51 +0530

    changes in module,tkwinSwitch,login

--- Login Page/Master Login Page - Draft 1.py
+++ Login Page/Master Login Page - Draft 1.py
@@ -4,7 +4,7 @@ from tkinter import *
 from tkinter.font import BOLD
 from PIL import Image, ImageTk
 from module import *
-
+from tkWinSwitch import *
 
 
 root=Tk()
@@ -24,44 +24,46 @@ def SubmitFn():
 root.geometry("1200x700")
 root.title("Password Manager - Login Page")
 
+set1 = wiSet()
 #setting background image
 bgImage=Image.open("Background Design - Password Manager.jpg") 
 bgImageTk= ImageTk.PhotoImage(bgImage)
 bgLabel = Label(image=bgImageTk)
-bgLabel.pack(fill=BOTH,expand=YES)
+set1.addWi(bgLabel)
+set1.pack(bgLabel,fill=BOTH,expand=TRUE)
 
 #login widget
-loginLabel=Label(bgLabel,bg="black")
-loginLabel.place(rely=0.5,relx=0.5,anchor=CENTER)
+loginLabel=set1.addWi(Label(bgLabel,bg="black"))
+set1.place(loginLabel,rely=0.5,relx=0.5,anchor=CENTER)
 
 #variables for fonts used
 usualfont_tuple=("Comic Sans MS",12,"bold")
 headFont_tuple=("Comic Sans MS",16,"bold")
 
 #Welcome Text 
-welText=Label(loginLabel, text="Welcome To Password Manager", font =headFont_tuple, bg="black",fg="white")
-welText.grid(row=0,column=1)
+welText=set1.addWi(Label(loginLabel, text="Welcome To Password Manager", font =headFont_tuple, bg="black",fg="white"))
+set1.grid(welText,row=0,column=1)
 
 #taking entries of username and password
-username=Label(loginLabel, text="Username: ",font=usualfont_tuple,bg="black",fg="white",pady=12,padx=12)
-password=Label(loginLabel,text="Paswword: ",font=usualfont_tuple,bg="black",fg="white")
-username.grid(row=1,column=0)
-password.grid(row=2,column=0)
+username=set1.addWi(Label(loginLabel, text="Username: ",font=usualfont_tuple,bg="black",fg="white",pady=12,padx=12))
+password=set1.addWi(Label(loginLabel,text="Paswword: ",font=usualfont_tuple,bg="black",fg="white"))
+set1.grid(username,row=1,column=0)
+set1.grid(password,row=2,column=0)
 
 uservalue=StringVar()
 passvalue=StringVar()
 
-userentry=Entry(loginLabel, textvariable=uservalue,width=40 )
-passentry=Entry(loginLabel,textvariable=passvalue,width=40)
-userentry.grid(row=1,column=1,padx=10)
-passentry.grid(row=2,column=1,padx=10)
+userentry=set1.addWi(Entry(loginLabel, textvariable=uservalue,width=40 ))
+passentry=set1.addWi(Entry(loginLabel,textvariable=passvalue,width=40))
+set1.grid(userentry,row=1,column=1,padx=10)
+set1.grid(passentry,row=2,column=1,padx=10)
 
 
 
 
 #Creating submit button
-submitBtn=Button(loginLabel, bg="grey", text="Submit",fg="white",command=SubmitFn)
-submitBtn.grid(row=3,column=1)
+submitBtn=set1.addWi(Button(loginLabel, bg="grey", text="Submit",fg="white",command=SubmitFn))
+set1.grid(submitBtn,row=3,column=1)
 
-
+set1.create()
 root.mainloop()
