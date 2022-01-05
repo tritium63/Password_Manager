e333a2964e9cb742a499bbba19efc10a3610a491
adinmadi <adinmadi@gmail.com>
2022-01-06 00:00:21 +0530

    Master Clear
    Signed-off-by: adinmadi <adinmadi@gmail.com>

--- Currently_Working/Enco_Deco.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-def encode(st):
-    b = bytes(st)
-    return base64.b64encode(b)
-def decode(b):
-    return base64.b64decode(b).decode()
--- Currently_Working/Pass_Gen.py
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 7113bea14792dda2b333c0e38c019ac90456d5cf
-import random, string
-def Pass_Gen():
-	x = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
-	return x
